8ed3182ba2510917f21ea91e381def3d831b59b2
Steven Serrano <serrano1314@gmail.com>
2023-11-12 17:44:56 +0800

    add private and public routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,32 @@
-import Login from "./Login";
-import Register from "./Register";
+import Login from "./component/Login";
+import Register from "./component/Register";
 import "./Style.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./component/Layout";
+import Home from "./component/Home";
+import RequireAuth from "./component/RequireAuth";
+import LinkPage from "./component/LinkPage";
 
 function App() {
   return (
     <>
-      {/* <Register /> */}
-      <Login />
+      <Routes>
+        {/* public routes  */}
+        <Route path="/" element={<Layout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="linkpage" element={<LinkPage />} />
+          <Route path="register" element={<Register />} />
+
+          {/* private routes  */}
+          <Route element={<RequireAuth />}>
+            <Route path="/" element={<Home />} />
+            <Route path="admin" element={"Admin Page"} />
+            <Route path="manager" element={"Manager Page"} />
+            <Route path="user" element={"User Page"} />
+          </Route>
+          <Route path="*" element={"404"} />
+        </Route>
+      </Routes>
     </>
   );
 }
