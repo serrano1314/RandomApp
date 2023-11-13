76ec6bb76124a76f8d05c16f1d3e818f17a48e17
Steven Serrano <serrano1314@gmail.com>
2023-11-13 15:09:25 +0800

    Update WebConfig.java

--- backend/src/main/java/tgsi/randomapp/backend/config/WebConfig.java
+++ backend/src/main/java/tgsi/randomapp/backend/config/WebConfig.java
@@ -16,12 +16,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 public class WebConfig {
+
     private static final Long MAX_AGE = 3600L;
     private static final int CORS_FILTER_ORDER = -102;
 
     @Bean
-    public FilterRegistrationBean corsFilter() {
-        System.out.println("WebConfig");
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
@@ -34,14 +34,17 @@ public class WebConfig {
                 HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
                 HttpMethod.PUT.name(),
+                HttpMethod.PATCH.name(),
                 HttpMethod.DELETE.name()));
         config.setMaxAge(MAX_AGE);
         source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
 
         // should be set order to -100 because we need to CorsFilter before
-
+        // SpringSecurityFilter
         bean.setOrder(CORS_FILTER_ORDER);
         return bean;
     }
+
 }
