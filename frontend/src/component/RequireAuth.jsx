c9ce048c15befa1ab222b86aed92d72f6928427f
Steven Serrano <serrano1314@gmail.com>
2023-11-13 15:11:04 +0800

    change ternary to if statement

--- frontend/src/component/RequireAuth.jsx
+++ frontend/src/component/RequireAuth.jsx
@@ -4,12 +4,12 @@ import useAuth from "../hooks/useAuth";
 const RequireAuth = ({ allowedRole }) => {
   const { auth } = useAuth();
   const location = useLocation();
-  return auth?.roles?.find((role) => allowedRole.includes(role)) ? (
-    <Outlet />
-  ) : auth?.user ? (
-    <Navigate to="/unauthorized" state={{ from: location }} replace />
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  );
+  if (auth?.roles?.find((role) => allowedRole.includes(role))) {
+    return <Outlet />;
+  } else if (auth?.user) {
+    return <Navigate to="/unauthorized" state={{ from: location }} replace />;
+  } else {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 };
 export default RequireAuth;
