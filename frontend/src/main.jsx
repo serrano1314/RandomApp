e1cdf97725b9bb165d97754766e48df86175af97
Steven Serrano <serrano1314@gmail.com>
2023-11-14 23:19:39 +0800

    add strictmode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,13 @@ import { AuthProvider } from "./context/AuthProvider.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </AuthProvider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
